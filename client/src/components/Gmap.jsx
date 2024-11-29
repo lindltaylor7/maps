50c729303b9220781a6d861a04253c7a302e6024
digitechgrouperu <digitechgrouperu@gmail.com>
2024-11-28 21:37:44 -0500

    next map

--- client/src/components/Gmap.jsx
+++ client/src/components/Gmap.jsx
@@ -14,7 +14,7 @@ const center = {
 const MyComponent = ({ long, lat, setValueLongitude, setValueLatitude }) => {
   const { isLoaded } = useJsApiLoader({
     id: "google-map-script",
-    googleMapsApiKey: process.env.REACT_APP_API_KEY_MAPS,
+    googleMapsApiKey: import.meta.env.VITE_REACT_APP_CLIENT_ID,
   });
 
   const [map, setMap] = useState(null);
