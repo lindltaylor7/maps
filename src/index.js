369e87461169418412e2c604434c69a0c43a3889
digitechgrouperu <digitechgrouperu@gmail.com>
2024-11-28 22:03:30 -0500

    next map env

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ const app = express();
 
 dotenv.config()
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://maps-vkuj.onrender.com/',
     credentials: true
 }))
 app.use(morgan('dev'))
